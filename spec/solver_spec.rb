a9530e6a6abf77725dcda82818ae19bf583627cc
olipliche <olipliche@gmail.com>
2021-11-25 23:47:54 +0100

    add fizzbuzz test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,10 +15,25 @@ describe Solver do
       expect(@solver.factorial(-4)).to eq 'Oh, there is an error. Please use a positive number'
     end
   end
-  
+
   context '#reverse' do
     it 'should return word reversed' do
       expect(@solver.reverser('Hello')).to eq 'olleH'
+    end
+  end
+
+  context '#fizzbuzz' do
+    it 'should return fizz when N is divisible BY 3' do
+      expect(@solver.fizzbuzz(3)).to eq 'fizz'
+    end
+    it 'should return buzz when N is divisible BY 5' do
+      expect(@solver.fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'should return fizzbuzz when N is divisible BY 3 and 5' do
+      expect(@solver.fizzbuzz(30)).to eq 'fizzbuzz'
+    end
+    it 'should return N when N is not divisible BY 3 and 5' do
+      expect(@solver.fizzbuzz(7)).to eq '7'
     end
   end
 end
