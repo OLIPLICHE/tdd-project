89b03492522d7d480150d72407c5f70c39b86d7b
olipliche <olipliche@gmail.com>
2021-11-25 23:48:23 +0100

    add fizzbuzz code

--- solver.rb
+++ solver.rb
@@ -6,10 +6,23 @@ class Solver
       num * factorial(num - 1)
     else
       'Oh, there is an error. Please use a positive number'
-    end    
+    end
   end
 
   def reverser(str)
-    str.reverse  
+    str.reverse
   end
+
+  def fizzbuzz(num)
+    if num.modulo(3).zero? && num.modulo(5).zero?
+      'fizzbuzz'
+    elsif num.modulo(3).zero?
+      'fizz'
+    elsif num.modulo(5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
+
 end
